636ae0298b8354399e234fd221a474f7fc80ef02
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-04-03 10:46:02 +0530

    Add files via upload

--- Programming/practice_2140.py
+++ Programming/practice_2140.py
@@ -17,11 +17,22 @@ print(function1(0, [[2,2],[3,1],[1,4]]))
 # dynamic programming approach: time complexity: O(n) and space complexity is O(n)
 
 array_2d = [[2,2],[3,1],[1,4]]
-len = len(array_2d)
 
-def function2(array_2d, len):
-    array = [0] * (len + 1)
 
-    for i in range(0, len):
-        array[i] = max(array_2d[i][0] + i + array_2d[i][1] + 1, i + 1)
+def function2(array_2d):
+    len_ = len(array_2d)
+    array = [0] * (len_ + 1)
 
+    for i in range(len_ - 1, -1, -1):
+        point, skip_index = array_2d[i]
+        next_index = i + skip_index + 1
+        take = point + (array[next_index] if next_index < len_ else 0)
+        skip = array[i + 1]
+
+        array[i] = max(take, skip)
+
+
+    return array[0]
+
+print(function2(array_2d))
+        
