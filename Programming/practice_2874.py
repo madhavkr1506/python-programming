2887ff48785823768031fae7aecc855aabd5ed22
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-04-07 15:15:22 +0530

    Add files via upload

--- Programming/practice_2874.py
+++ Programming/practice_2874.py
@@ -1,16 +1,25 @@
+import math
+
 def function1(array):
     len_ = len(array)
-    i_max = []
+    i_max = [0] * len_
+    i_max[0] = (array[0])
     for i in range(1, len_, 1):
-        i_max.append(max(array[i-1], array[i]))
-
-    print(i_max)
-
-    j_max = []
-    for i in range(len_-2, -1, -1):
-        j_max.append(max(array[i + 1], array[i]))
-
-    print(j_max)
+        i_max[i] = (max(i_max[i-1], array[i]))
 
 
-function1([2,1,4,5,3])
+    j_max = [0] * len_
+    j_max[-1] = array[-1]
+    for j in range(len_-2, -1, -1):
+        j_max[i] = (max(j_max[j + 1], array[j]))
+
+    max_value = -math.inf
+    for i in range(1, len_ - 2, 1):
+        max_value = max(i_max[i - 1] - array[i] * j_max[i + 1], max_value)
+
+    print(max_value)
+
+
+
+
+function1([12,6,1,2,7])
