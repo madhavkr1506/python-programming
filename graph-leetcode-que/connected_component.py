76add7e00e3d1169bd0d4ed325d24dc74aa39381
madhavkr1506 <madhavkr9153276724@gmail.com>
2025-08-27 18:21:24 +0530

    connected components count problem solved

--- graph-leetcode-que/connected_component.py
+++ graph-leetcode-que/connected_component.py
@@ -2,7 +2,8 @@ from queue import Queue
 
 def main():
     try:
-        conn_components = graph_function()
+        # conn_components = graph_function()
+        conn_components = dsu_conn_components()
         print(f"Connected components => {conn_components}")
     except Exception as e:
         print(f"Message: {e}")
@@ -44,6 +45,43 @@ def graph_function():
         raise e
     
 # TC: (N X E)
+
+def dsu_conn_components():
+    try:
+        edges = [[0,1],[1,2],[3,4]]
+        nodes = 5
+        parents = [node for node in range(nodes)]
+
+        for edge in edges:
+            node1, node2 = edge
+            union(node1, node2, parents)
+        conn_components = 1
+        for node in range(len(parents)):
+            if node > 0:
+                if parents[node - 1] == parents[node]:
+                    continue
+                conn_components += 1
+        return conn_components
+
+    except Exception as e:
+        raise e
     
+def union(node1, node2, parents):
+    try:
+        parent_node1 = find(node1, parents)
+        parent_node2 = find(node2, parents)
+        parents[parent_node2] = parent_node1
+    except Exception as e:
+        raise e
+    
+def find(node, parents):
+    try:
+        if parents[node] == node:
+            return node
+        return find(parents[node], parents)
+    except Exception as e:
+        raise e
+
+
 if __name__ == "__main__":
     main()
