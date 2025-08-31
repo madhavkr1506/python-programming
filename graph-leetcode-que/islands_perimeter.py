2a5e5e37d8e3f10bdfc18fe2c2a77928e08bad70
madhavkr1506 <madhavkr9153276724@gmail.com>
2025-08-31 21:01:13 +0530

    islands perimeter problem solved

--- graph-leetcode-que/islands_perimeter.py
+++ graph-leetcode-que/islands_perimeter.py
@@ -1,6 +1,7 @@
 def main():
     try:
-        utility_function()
+        perimeter = utility_function()
+        print(f"Perimeter: {perimeter}")
     except Exception as e:
         print(f"Message: {e}")
 
@@ -10,16 +11,23 @@ def utility_function():
 
         rows = len(grid)
         cols = len(grid[0])
-        count1 = 0
-        poss_x = [-1,0,1,0]
-        poss_y = [0,1,0,-1]
+        peri = 0
+        poss_x = [1,0]
+        poss_y = [0,-1]
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 1:
-                    
-
-        
-        print(count1)
+                if grid[row][col] == 0:
+                    continue
+                elif grid[row][col] == 1:
+                    peri += 4
+                    for k in range(2):
+                        new_x = row + poss_x[k]
+                        new_y = col + poss_y[k]
+                        if new_x < 0 or new_x >= rows or new_y < 0 or new_y >=cols:
+                            continue
+                        if grid[new_x][new_y] == 1:
+                            peri -= 2
+        return peri
 
     except Exception as e:
         raise e
