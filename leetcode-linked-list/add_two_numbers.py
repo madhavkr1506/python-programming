9d03a4c2f402b5295594a92b97544201ba8817f0
madhavkr1506 <madhavkr9153276724@gmail.com>
2025-09-21 20:54:55 +0530

    problem add two numbers solved

--- leetcode-linked-list/add_two_numbers.py
+++ leetcode-linked-list/add_two_numbers.py
@@ -18,15 +18,20 @@ def main():
 
 def utility_function(head1, head2):
     try:
-        total, carry = 0, 0
+        carry = 0
         curr1 = head1
         curr2 = head2
         dummy = node(-1)
         dummynode = dummy
         while curr1 is not None or curr2 is not None or carry != 0:
-            total = (curr1.value + curr2.value + carry)
-            curr1 = curr1.next
-            curr2 = curr2.next
+            total = 0
+            if curr1:
+                total += curr1.value
+                curr1 = curr1.next
+            if curr2:
+                total += curr2.value
+                curr2 = curr2.next
+            total += carry
             digit = total % 10
             carry = total // 10
             dummynode.next = node(digit)
