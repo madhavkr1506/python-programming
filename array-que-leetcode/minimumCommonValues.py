6e138bc0f93ac3a69bef7cc0271e8aab44912328
madhavkr1506 <madhavkr9153276724@gmail.com>
2026-05-19 19:36:42 +0530

    minimum Common Values. fixed infinite loop condition

--- array-que-leetcode/minimumCommonValues.py
+++ array-que-leetcode/minimumCommonValues.py
@@ -9,12 +9,15 @@ def getCommon(nums1 : list[int], nums2 : list[int]) -> int:
     try:
         n1, n2 = len(nums1), len(nums2)
         if n1 == 0 or n2 == 0: raise EmptyListException(f"either of list is empty or both list are empty. list1: {nums1} or list2: {nums2}")
-        if n1 > n2: return getCommon(nums1=nums2, nums2=nums1)
+        if n1 > n2: 
+            nums1, nums2 = nums2, nums1
+            n1, n2 = n2, n1
+            return getCommon(nums1=nums2, nums2=nums1)
 
         for x in nums2:
             left, right = 0, n1-1
             while left <= right:
-                mid = (left + (right - left)) // 2
+                mid = left + (right - left) // 2
                 if nums1[mid] == x:
                     return x
                 if nums1[mid] > x: right = mid - 1
@@ -27,7 +30,7 @@ def getCommon(nums1 : list[int], nums2 : list[int]) -> int:
     
 def main():
     try:
-        nums1 = [1, 2, 3]
+        nums1 = [1, 2]
         nums2 = [2, 4]
         """
         both list is sorted in non decreasing order
