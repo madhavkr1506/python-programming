5a23fe2c45c480b6d636709a8f9fe65e2413254f
madhavkr1506 <madhavkr9153276724@gmail.com>
2026-06-03 00:03:08 +0530

    valid sudoku

---
+++ array-que-leetcode/validSudoku.py
@@ -0,0 +1,91 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+
+"""
+
+board = [["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+rows, cols = 9, 9
+
+for row in range(rows):
+    for col in range(cols):
+        if board[row][col] == ".":
+            continue
+        val = (int)(board[row][col])
+        for m in range(rows):
+            if m == row: continue
+            if (board[m][col]) == ".": continue
+            if (int)(board[m][col]) == val:
+                print("val found")
+                exit()
+        for n in range(cols):
+            if n == col: continue
+            if (board[row][n]) == ".": continue
+            if (int)(board[row][n]) == val:
+                print("val found")
+                exit()
+        inner_grid_row = row - (row % 3)
+        inner_grid_col = col - (col % 3)
+        for i in range(3):
+            for j in range(3):
+                if i + inner_grid_row == row and j + inner_grid_col == col: continue
+                if (board[i + inner_grid_row][j + inner_grid_col]) == ".": continue
+                temp = (int)(board[i + inner_grid_row][j + inner_grid_col])
+                if val == temp:
+                    print(f"val found")
+                    exit()
+print(f"valid sudoku")
+
